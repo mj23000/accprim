6b697fca341511bb7708cf9854ef9a54f9824233
mj23000 <markusjacobsen@hotmail.com>
2017-01-23 21:57:00 +0100

    w

--- accprim.js
+++ accprim.js
@@ -56,6 +56,7 @@ document.getElementById('accelerometer').innerHTML =
 'Acceleration X: ' + accX + '<br />' +
 'Acceleration Y: ' + accY + '<br />' +
 'Acceleration Z: ' + accZ + '<br />' +
+'primtal       : ' +  j   + '<br />' +
 'Timestamp	   : ' + timestamp + '<br />' ;
 	}
 }
